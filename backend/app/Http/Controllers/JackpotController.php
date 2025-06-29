7839cc9f4c185233130c14da007770f9dc780e1f
bramz-00 <zakaria.braham@outlook.com>
2025-06-29 20:47:58 +0100

    Add initial tables with thier files generated jackpot-jackpot-contribution / event - orders - tickets / volunteer-opportunity-application  / good-request

---
+++ backend/app/Http/Controllers/JackpotController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreJackpotRequest;
+use App\Http\Requests\UpdateJackpotRequest;
+use App\Models\Jackpot;
+
+class JackpotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJackpotRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jackpot $jackpot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jackpot $jackpot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJackpotRequest $request, Jackpot $jackpot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jackpot $jackpot)
+    {
+        //
+    }
+}
---
+++ backend/app/Models/VolunteerOpportunity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VolunteerOpportunity extends Model
+{
+    /** @use HasFactory<\Database\Factories\VolunteerOpportunityFactory> */
+    use HasFactory;
+}
