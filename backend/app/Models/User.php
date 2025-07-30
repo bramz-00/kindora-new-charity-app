3922d2f6a2e044828476c07f2b177439e99e8b22
Zakaria <zakaria.braham@outlook.com>
2025-07-30 21:43:35 +0100

    add spatie package to project

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 // implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes,HasRoles;
 
     /**
      * The attributes that are mass assignable.
